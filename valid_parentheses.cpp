1a7732b26b33a6498cebe0ce0f1e12599dfd55c1
Pratiksha-Andhare-PA <128924098+Pratiksha-Andhare-PA@users.noreply.github.com>
2023-07-07 09:08:35 +0530

    solution

---
+++ valid_parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        for(int i=0;i<s.length();i++){
+            if(st.empty()){
+                st.push(s[i]);
+            }
+            else if(st.top()=='(' && s[i]==')' || st.top()=='[' && s[i]==']' || st.top()=='{' && s[i]=='}'){
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+        if(!st.empty()){
+            return false;
+        }
+        return true;
+    }
+};
